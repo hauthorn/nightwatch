64fb5cdf454927da0e8f2694d4d53e5e205550cc
Christoffer Berg Boisen <christoffer@emplate.it>
2025-06-19 08:39:47 +0200

    Align UserProviderTest with how the provider is actually instantiated in NightwatchServiceProvider

--- tests/Unit/UserProviderTest.php
+++ tests/Unit/UserProviderTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use Illuminate\Auth\GenericUser;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Nightwatch\UserProvider;
 use Tests\TestCase;
@@ -17,7 +18,7 @@ class UserProviderTest extends TestCase
         Auth::login(new GenericUser([
             'id' => str_repeat('x', 1000),
         ]));
-        $provider = new UserProvider($this->app['auth'], fn () => []);
+        $provider = new UserProvider($this->app['auth'], fn () => fn (Authenticatable $user) => []);
 
         $this->assertSame(1000, strlen(Auth::id()));
         $this->assertSame($provider->id(), str_repeat('x', 255));
@@ -25,7 +26,7 @@ class UserProviderTest extends TestCase
 
     public function test_it_can_lazily_retrieve_the_user(): void
     {
-        $provider = new UserProvider($this->app['auth'], fn () => []);
+        $provider = new UserProvider($this->app['auth'], fn () => fn (Authenticatable $user) => []);
 
         $id = $provider->id();
 
@@ -38,7 +39,7 @@ class UserProviderTest extends TestCase
 
     public function test_it_can_remember_an_authenticated_user_and_limits_the_length_of_their_identifier(): void
     {
-        $provider = new UserProvider($this->app['auth'], fn () => []);
+        $provider = new UserProvider($this->app['auth'], fn () => fn (Authenticatable $user) => []);
         $provider->remember($user = new GenericUser([
             'id' => str_repeat('x', 1000),
         ]));
